ce0c5111d464703c4c503cbd84e3e96e2f35a9a7
matteoLorenzini <matteo-lorenzini@outlook.it>
2021-11-28 12:22:11 +0100

    Updated index and target file

--- OXFAM/excel2csv.py
+++ OXFAM/excel2csv.py
@@ -11,9 +11,13 @@ csv = df1[['Indicator ID', 'Indicator name','Company','Country ISO','Country nam
 
 print(csv)
 
-mapping = csv.rename(columns={	'Indicator ID': 'ID','Indicator name': 'Title',
+mapping = csv.rename(columns={	'Indicator ID': 'IndicatorID','Indicator name': 'Title',
 								'Company': 'Company','Country ISO': 'Country/ISO','Country name': 'Country/Name',
-								'Time': 'Time','Value': 'Value','Note': 'Note'}, index={'Indicator ID': 'ID'})
+								'Time': 'Time','Value': 'Value','Note': 'Note'}, index={'Indicator ID': 'IndicatorID'})
+
+mapping['ID'] =  range(1, 1+len(mapping))
+
+mapping['ID'] = 'oxfam:' + mapping['ID'].astype(str)
 
 print(mapping)
 
